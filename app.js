e59b90dabe0999973bd65fca96cf14c8921cb841
leonelcarreras <leocarrerassteam@hotmail.com>
2022-03-24 19:07:00 -0300

    mercado liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.listen(4001, () => console.log("running on port 4001"));
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+app.get("/product", (req, res) => {
+  res.sendFile(__dirname + "/views/product.html");
+});
+
